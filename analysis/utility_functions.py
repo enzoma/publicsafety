df13b9a0147f1d17c2a8df6220a000dd468a65b4
mcfowland <mcfowland@cmu.edu>
2013-07-08 19:22:22 +0000

    updated utility_functions.py to create a get_db_connection function and now have get mk_data_frame_from_sql_query to call that function

--- analysis/utility_functions.py
+++ analysis/utility_functions.py
@@ -4,8 +4,8 @@ import db_setup
 import psycopg2
 import pandas.io.sql as sqlio
 
-def mk_data_frame_from_sql_query(sql_query, db_conn=None):
-
+## Helper function to create (if necessary) a connection to the database
+def get_db_conn(db_conn=None):
   if db_conn == None:
     # Make a connection to the database
     if db_setup.DB_PASSWORD == None:
@@ -19,6 +19,12 @@ def mk_data_frame_from_sql_query(sql_query, db_conn=None):
         user     = db_setup.DB_USER,\
         host     = db_setup.DB_HOST,\
         password = db_setup.DB_PASSWORD)
+  return db_conn
 
+def mk_data_frame_from_sql_query(sql_query, db_conn=None):
+  '''Fetches rows corresponding the provided sql query, places it
+  into a data frame, and return the data frame'''
   # Create a dataframe that consists of the data defined by our SQL
-  return(sqlio.read_frame(sql_query, db_conn))
+  return sqlio.read_frame(sql_query, get_db_conn(db_conn))
+
+
