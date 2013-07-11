dee670c3106edc94283495decef5a741ac5af0d0
tplagge <tplagge@gmail.com>
2013-07-11 05:50:40 +0000

    Import jail data into database

---
+++ sql_import_scripts/create_jail_table.sql
@@ -0,0 +1,32 @@
+DROP SCHEMA jail CASCADE;
+
+CREATE SCHEMA jail;
+
+SET search_path TO jail,public;
+
+CREATE SEQUENCE jail.jail_seq;
+
+CREATE TABLE jail.inmates (
+  id             INTEGER NOT NULL DEFAULT nextval('jail_seq'),
+  inmate_name    VARCHAR,
+  birth_date     TIMESTAMP, 
+  booking_date   TIMESTAMP,
+  address1       VARCHAR,
+  address2       VARCHAR,
+  city           VARCHAR,
+  state          VARCHAR,
+  country        VARCHAR,
+  release_date   TIMESTAMP,
+  charge_code    VARCHAR,
+  charge_desc    VARCHAR,
+  current_bond   FLOAT
+);
+
+ALTER SEQUENCE jail.jail_seq 
+  OWNED BY jail.inmates.id;
+ALTER TABLE ONLY jail.inmates
+  ADD CONSTRAINT jail_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY jail.inmates
+  ADD CONSTRAINT jail_unique UNIQUE(id);
+
+\copy jail.inmates (inmate_name, birth_date, booking_date, address1, address2, city, state, country, release_date, charge_code, charge_desc, current_bond) FROM '/mnt/data1/CPD/jail/JailData1.csv' CSV HEADER
