8b0ccf1ddc48c456ee95a58f3e2053c8f7d083e8
vbashyakarla <vbashyakarla@ip-10-249-67-162.us-west-2.compute.internal>
2013-07-16 09:06:25 +0000

    Updated Jail Data Analysis having created uniformly labelled crime codes.  The code also includes updated scripts on creating the ranked ILCS crime code CDF. git push

--- analysis/Jail Analysis.R
+++ analysis/Jail Analysis.R
@@ -1,9 +1,8 @@
-# Jail Analysis
+############ Jail Analysis ###########
 setwd("~/Data Science for Social Good/CPD/Crime Data/Jail Data")
 
-##############
-# Data Import
-##############
+
+### Data Import ####
 # NB: These file paths specify my local directory, but the relevant csv files are on the server
 JailData1 <- read.csv("~/Data Science for Social Good/CPD/Crime Data/Jail Data/JailData1.csv")
 View(JailData1)
@@ -13,10 +12,11 @@ JailData3 <- read.csv("~/Data Science for Social Good/CPD/Crime Data/Jail Data/J
 View(JailData3)
 JD_1_RData <- read.csv("~/Data Science for Social Good/CPD/Crime Data/Jail Data/JailData1_forR.csv")
 View(JD_1_RData)
+Cleaning_Jail_Data <- read.csv("~/Data Science for Social Good/CPD/Crime Data/Jail Data/Cleaning_Jail_Data.csv")
+data = Cleaning_Jail_Data
+ 
 
-###############################################################
-# General Data Housekeeping and determining which dataset to use
-###############################################################
+##### General Data Housekeeping and determining which dataset to use ####
 names(JailData1)
 names(JailData2)
 names(JailData3)
@@ -31,10 +31,7 @@ length(intersect(JD1_names, JD2_names))
 length(intersect(JD1_names, JD3_names))
 length(intersect(JD2_names, JD3_names))
 
-#############################################
-# NB: Decided to focus analysis on JD_1_RData
-#############################################
-
+### NB: Decided to focus analysis on JD_1_RData, later updated to "data" ####
 # Determine Counts for each unique name in JD_1_RData
 # NB: This took several hours to run, so I've uploaded a csv
 # file with the output to the server.  See Arrest_Counts.csv
@@ -48,9 +45,7 @@ for(i in 1:length(unique(JD_1_RData$InmateName))){
   if(i%%100==0) cat(i, print(Sys.time()))
 }
 
-## ## ## ## ## ## ## ## ## ## ## ## 
-## Number of Arrests Exploration
-## ## ## ## ## ## ## ## ## ## ## ## 
+#### Number of Arrests Exploration #########
 # Histograms of Number of Arrests
 par(new = FALSE)
 
@@ -78,9 +73,56 @@ hist(arrest.count[which(arrest.count>30)],
      main = "Histogram for Individuals with > 30 Previous Arrests ", col = "lavender")
 mtext("Cook County Sheriff's Office Jail Data")
 
-### ILCS Code Analysis
-plotdata = cumsum((as.numeric(rev(sort(table(data$ChargeCode)))))/sum(as.numeric(table(data$ChargeCode))))
-ggplot(plotdata, aes(x=x.to.plot, y=y.to.plot)) +
-  geom_point(shape=1, colour = "red") + labs(title="Proportion of Cumulative Ranked ILCS Crime Codes") +
+######## ILCS Code Analysis #######
+# Create a table 
+charge.code.table = cbind(sort(table(data$ChargeCode), decreasing = TRUE))
+indiv.prop = 
+  as.numeric(rev(sort(table(data$ChargeCode))))/sum(as.numeric(table(data$ChargeCode)))
+cumulative.prop = 
+  cumsum(as.numeric(rev(sort(table(data$ChargeCode)))))/sum(as.numeric(table(data$ChargeCode)))
+charge.code.table = cbind(charge.code.table, indiv.prop, cumulative.prop)
+
+# Save table to CSV
+write.csv(charge.code.table, file = "Crime_Codes_of_Interest.csv", 
+          col.names = TRUE)
+
+# Get Descriptions of these Crime Codes
+Crime_Codes_of_Interest =
+  read.csv("~/Data Science for Social Good/CPD/Crime Data/Jail Data/Crime_Codes_of_Interest.csv")
+crime.codes = Crime_Codes_of_Interest
+
+description=character()
+for(k in 1:dim(crime.codes)[1]){
+  description[k] = (as.character((data$ChargeDescription[which(data$ChargeCode==
+    paste(as.character(crime.codes$Crime.Code[k])))][1])))
+  if(k%%100==0) print(k)
+}
+
+# Now append these to the crime.codes table
+crime.codes = cbind(charge.code.table, description)
+
+# Save this new table
+write.csv(crime.codes, file = "Crime_Codes_of_Interest_Final.csv",
+          col.names = TRUE)
+
+# Create a Graph
+library(ggplot2)
+plotdata = as.data.frame(cbind(x = seq(1:length(cumulative.prop)), y = cumulative.prop))
+ggplot(plotdata, aes(x=x, y=cumulative.prop)) +
+  geom_point(shape=1, colour = "navy") + labs(title="Proportion of Cumulative Ranked ILCS Crime Codes") +
   xlab("Code Rank (by Frequency)") +
   ylab("Cumulative Proportion of All Codes in Dataset")
+
+
+
+
+## ## ## ## ## ## ## #### ## ## #### ## ## #### ## ## #### ## ## #### ## ## ##
+# TO DO: See if the charge codes/descriptions for the big criminals 
+# (ie, > 30 previous arrests) are the same or if these vary.  Also, it'd be interesting
+# to see if their bonds increase after getting arrested more and more times
+
+### Bond Amounts
+# Comparison of Overall Non-0 Bond Amounts of those with low vs. high arrest count
+hist(JD_1_RData$CurrentBond[which(JD_1_RData$CurrentBond!=0)])
+## ## ## #### ## ## #### ## ## #### ## ## #### ## ## #### ## ## #### ## ## ##
+
